6d0cc02640c6ad8a909736a0415cf95f8e43f693
sw1308 <sw1308@york.ac.uk>
2014-12-26 15:54:37 +0000

    Continued work on Practical2

--- Haskell/Practical2.hs
+++ Haskell/Practical2.hs
@@ -1,29 +1,32 @@
-type RegExp a :: [a] -> (Maybe [a], [a])
+import Data.Maybe
+
+type RegExp a = [a] -> (Maybe [a], [a])
 
 nil :: RegExp a
 nil s = (Just [], s)
 
 range :: (a -> Bool) -> RegExp a
-range r a
-	| r p@(x:xs) == True = (Just [x], xs)
+range r p@(x:xs)
+	| r x == True = (Just [x], xs)
 	| otherwise = (Nothing, p)
 
 one :: Eq a => a -> RegExp a
 one a = range (==a)
 
 arb :: RegExp a
-arb a = range (const True)
+arb = range (const True)
 
-alt :: Eq a => RegExp a -> RegExp a -> RegExp a
-alt r0 r1 s
+alt, sqn :: Eq a => RegExp a -> RegExp a -> RegExp a
+(r0 `alt` r1) s
 	| isJust m = t
-	| otherwise r1 s
-	where t@(m, rs) = r0 s
-
-sqn :: Eq a => RegExp a -> RegExp a -> RegExp a
-sqn r0 r1 s
-	| isJust m0 && isJust m1 = (Just (fromJust m0 ++ fromJust m1), rs)
-	| otherwise = t
+	| otherwise = r1 s
+	where t@(m,rs) = r0 s
+(r0 `sqn` r1) s
+	| isJust m0 && isJust m1 = (Just (fromJust m0 ++ fromJust m1), rs1)
+	| otherwise = (Nothing, s)
 	where
-		(m0, ts) = r0 s
-		(m1, rs) = r1 ts
+		(m0, rs0) = r0 s
+		(m1, rs1) = r1 rs0
+
+sqns :: Eq a => [a] -> RegExp a
+sqns = (foldl sqn nil).(map one)
