89e4903f514ec28f30a6fe22cfeb1dcd4e0485d5
sw1308 <sw1308@york.ac.uk>
2015-01-19 18:24:02 +0000

    Began work on gengine.c

--- C/src/gengine.c
+++ C/src/gengine.c
@@ -5,7 +5,16 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-void init(int width, int height)
+void init(int width, int height, char *windowTitle)
 {
 	
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	srand(time(NULL));
+	glutInitWindowSize(width, height);
+	glutInitWindowPosition(50,50)
+	glutCreateWindow(windowTitle);
 }
