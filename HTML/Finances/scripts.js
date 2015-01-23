18cc48e77984896bd3a3399b394bc16c66ed9d4c
sw1308 <sw1308@york.ac.uk>
2015-01-23 18:23:06 +0000

    Completed Prototype for Finance page design

--- HTML/Finances/scripts.js
+++ HTML/Finances/scripts.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
 	$(".hiddenTop").removeClass("hiddenTop");
-	
+	$(".comment").addClass("selectable");
+	$(".inOut").addClass("selectable");
+	$(".amount").addClass("selectable");
+	$(".selectable").parents().css("overflow", "hidden");
 	var parent, ink, d, x, y;
 	$(".selectable").click(function(e){
 		parent = $(this).parent();
 		//create .ink element if it doesn't exist
 		if(parent.find(".ink").length == 0)
 			parent.prepend("<span class='ink'></span>");
-		
+
 		ink = parent.find(".ink");
 		//incase of quick double clicks stop the previous animation
 		ink.removeClass("animate");
-	
+
 		//set size of .ink
 		if(!ink.height() && !ink.width())
 		{
@@ -19,13 +22,13 @@ $(document).ready(function(){
 			d = Math.max(parent.outerWidth(), parent.outerHeight());
 			ink.css({height: d, width: d});
 		}
-	
+
 		//get click coordinates
 		//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
-		x = e.pageX - parent.offset().left - ink.width()/2;
-		y = e.pageY - parent.offset().top - ink.height()/2;
-	
+		x = e.pageX - ink.width()/2;
+		y = e.pageY - ink.height()/2;
+
 		//set the position and add class .animate
 		ink.css({top: y+'px', left: x+'px'}).addClass("animate");
-	})
+	});
 });
