1edc1efd4837c9e3e7f78fc3fe72e9ade4981a20
sw1308 <sw1308@york.ac.uk>
2015-02-07 19:18:53 +0000

    Began SYAC1

--- C/src/SYAC1.c
+++ C/src/SYAC1.c
@@ -1,55 +1,37 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <strings.h>
 
-main()
+int main(int argc, char *argv[])
 {
-	int fd1, fd2;
-
-	fd1 = open("in1.txt", O_RDONLY);
-	if (fd1 < 0)
-	{
-		perror("c1"); exit(1);
-	}
-
-	fd2 = open("in1.txt", O_RDONLY);
-	if (fd2 < 0)
-	{
-		perror("c1"); exit(1);
-	}
-
-	printf("Opened the file in1.txt twice: Fd's are %d and %d.\n", fd1, fd2);
-
-	if (close(fd1) < 0)
-	{
-		perror("c1"); exit(1);
-	}
+	int fd;
+	char c;
 	
-	if (close(fd2) < 0)
+	printf("This");
+	
+	while(read(0, &c, 1) == 1)
 	{
-		perror("c1"); exit(1);
-	}
-
-	printf("Closed both fd's.\n");
-
-	fd2 = open("in1.txt", O_RDONLY);
-	if (fd2 < 0)
-	{
-		perror("c1"); exit(1);
-	}
-	  
-	printf("Reopened in1.txt into fd2: %d.\n", fd2);
-
-	if (close(fd2) < 0)
-	{
-		perror("c1"); exit(1);
-	}
-
-	printf("Closed fd2. Now, calling close(fd2) again.\n");
-	printf("This should cause an error.\n\n");
-
-	if (close(fd2) < 0)
-	{
-		perror("c1"); exit(1);
+		if(c == 'o')
+		{
+			char *name;
+			
+			name = (char *) calloc(128, sizeof(char));
+			
+			while(read(0, &c, 1) == 1)
+			{
+				strcat(name, &c);
+			}
+			
+		}
+		else if(c == 'q')
+		{
+			close(fd);
+			exit(0);
+		}
+		else
+		{
+			printf("Please enter o or q");
+		}
 	}
 }
