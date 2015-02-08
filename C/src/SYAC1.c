cc22dbbab09d1cc66a677fea44b97436c2e5c717
sw1308 <sw1308@york.ac.uk>
2015-02-08 15:34:20 +0000

    SYAC1

--- C/src/SYAC1.c
+++ C/src/SYAC1.c
@@ -12,26 +12,25 @@ int main(int argc, char *argv[])
 	
 	while(read(0, &c, 1) == 1)
 	{
-		if(c == 'o')
-		{
-			char *name;
-			
-			name = (char *) calloc(128, sizeof(char));
-			
-			while(read(0, &c, 1) == 1)
-			{
-				strcat(name, &c);
-			}
-			
-		}
-		else if(c == 'q')
-		{
-			close(fd);
-			exit(0);
-		}
-		else
-		{
-			printf("Please enter o or q");
-		}
+/*		if(c == 'o')*/
+/*		{*/
+/*			char *name;*/
+/*			*/
+/*			name = (char *) calloc(128, sizeof(char));*/
+/*			*/
+/*			while(read(0, &c, 1) == 1)*/
+/*			{*/
+/*				strcat(name, &c);*/
+/*			}*/
+/*		}*/
+/*		else if(c == 'q')*/
+/*		{*/
+/*			close(fd);*/
+/*			exit(0);*/
+/*		}*/
+/*		else*/
+/*		{*/
+/*			printf("Please enter o or q");*/
+/*		}*/
 	}
 }
