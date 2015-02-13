eda7d5e82522835f8b2aac85ef832fb688b714b7
sw1308 <sw1308@york.ac.uk>
2015-02-13 13:55:46 +0000

    Sync push

--- C/src/SYAC1.c
+++ C/src/SYAC1.c
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,25 +14,24 @@ int main(int argc, char *argv[])
 	
 	while(read(0, &c, 1) == 1)
 	{
-/*		if(c == 'o')*/
-/*		{*/
-/*			char *name;*/
-/*			*/
-/*			name = (char *) calloc(128, sizeof(char));*/
-/*			*/
-/*			while(read(0, &c, 1) == 1)*/
-/*			{*/
-/*				strcat(name, &c);*/
-/*			}*/
-/*		}*/
-/*		else if(c == 'q')*/
-/*		{*/
-/*			close(fd);*/
-/*			exit(0);*/
-/*		}*/
-/*		else*/
-/*		{*/
-/*			printf("Please enter o or q");*/
-/*		}*/
+		if(c == 'q')
+		{
+			exit(0);
+		}
+		
+		if(c == 'o')
+		{
+			int stringLength;
+			write(1, "o was pressed\n", strlen("o was pressed\n"));
+			stringLength = SEEK_END - SEEK_CUR;
+			
+			char *args;
+			
+			args = (char *) calloc(stringLength, sizeof(char));
+			
+			read(0, args, stringLength);
+			
+			write(1, args, stringLength);			
+		}
 	}
 }
--- C/src/colorTest.c
+++ C/src/colorTest.c
@@ -1,7 +1,8 @@
 /*
  *TO COMPILE WITH GCC USE:
- * gcc src/colorTest.c -o bin/colorTest -lGL -lGLU -lglut
+ * gcc src/colorTest.c -o bin/colorTest -lGL -lGLU -lglut -lm
  */
+#include <math.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,10 +12,12 @@
 
 #define TRUE 1
 #define FALSE 0
+#define INC 0.01f
 
-int colorCase;
+int colorCase, rComplete, bComplete, gComplete;
 int keyStates[256];
 float r, g, b;
+float v[3];
 
 void draw()
 {
@@ -25,59 +28,134 @@ void draw()
 	glutPostRedisplay();
 }
 
+void generateV()
+{
+	v[0] = (float) rand()/(float)RAND_MAX;
+	v[1] = (float) rand()/(float)RAND_MAX;
+	v[2] = (float) rand()/(float)RAND_MAX;
+	v[0] = roundf(v[0] * 100) / 100;
+	v[1] = roundf(v[1] * 100) / 100;
+	v[2] = roundf(v[2] * 100) / 100;
+}
+
+/*void redraw()*/
+/*{*/
+/*	glutTimerFunc(16, redraw, 0);*/
+/*	*/
+/*	switch(colorCase)*/
+/*	{*/
+/*		case 0:*/
+/*			if(r > g)*/
+/*			{*/
+/*				g += 0.01f;*/
+/*			}*/
+/*			else if (r > 0.0f)*/
+/*			{*/
+/*				r -= 0.01f;*/
+/*			}*/
+/*			else*/
+/*			{*/
+/*				colorCase = 1;*/
+/*			}*/
+/*			break;*/
+/*		case 1:*/
+/*			if(g > b)*/
+/*			{*/
+/*				b += 0.01f;*/
+/*			}*/
+/*			else if (g > 0.0f)*/
+/*			{*/
+/*				g -= 0.01f;*/
+/*			}*/
+/*			else*/
+/*			{*/
+/*				colorCase = 2;*/
+/*			}*/
+/*			break;*/
+/*		case 2:*/
+/*			if (b > r)*/
+/*			{*/
+/*				r += 0.01f;*/
+/*			}*/
+/*			else if (b > 0.0f)*/
+/*			{*/
+/*				b -= 0.01f;*/
+/*			}*/
+/*			else*/
+/*			{*/
+/*				colorCase = 0;*/
+/*			}*/
+/*			break;*/
+/*		default:*/
+/*			r = 1.0f;*/
+/*			g = 0.0f;*/
+/*			b = 0.0f;*/
+/*			colorCase = 0;*/
+/*	}*/
+/*	*/
+/*	glClearColor(r, g, b, 1.0f);*/
+/*	*/
+/*	glClear(GL_COLOR_BUFFER_BIT);*/
+/*	*/
+/*	glFlush();*/
+/*	glutPostRedisplay();*/
+/*}*/
+
 void redraw()
 {
 	glutTimerFunc(16, redraw, 0);
 	
-	switch(colorCase)
+	if(r > v[0])
 	{
-		case 0:
-			if(r > g)
-			{
-				g += 0.01f;
-			}
-			else if (r > 0.0f)
-			{
-				r -= 0.01f;
-			}
-			else
-			{
-				colorCase = 1;
-			}
-			break;
-		case 1:
-			if(g > b)
-			{
-				b += 0.01f;
-			}
-			else if (g > 0.0f)
-			{
-				g -= 0.01f;
-			}
-			else
-			{
-				colorCase = 2;
-			}
-			break;
-		case 2:
-			if (b > r)
-			{
-				r += 0.01f;
-			}
-			else if (b > 0.0f)
-			{
-				b -= 0.01f;
-			}
-			else
-			{
-				colorCase = 0;
-			}
-			break;
-		default:
-			r = 1.0f;
-			g = 0.0f;
-			b = 0.0f;
-			colorCase = 0;
+		r -= INC;
+	}
+	else if(r < v[0])
+	{
+		r += INC;
+	}
+	else
+	{
+		rComplete = TRUE;
+	}
+	
+	if(g > v[1])
+	{
+		g -= INC;
+	}
+	else if(g < v[1])
+	{
+		g += INC;
+	}
+	else
+	{
+		gComplete = TRUE;
+	}
+	
+	if(b > v[2])
+	{
+		b -= INC;
+	}
+	else if(b < v[2])
+	{
+		b += INC;
+	}
+	else
+	{
+		bComplete = TRUE;
+	}
+	
+	r = roundf(r * 100) / 100;
+	g = roundf(g * 100) / 100;
+	b = roundf(b * 100) / 100;
+	
+	printf("r: %f g: %f b: %f v: %f, %f, %f\n", r, g, b, v[0], v[1], v[2]);
+	
+	if(rComplete == TRUE && gComplete == TRUE && bComplete == TRUE)
+	{
+		generateV();
+		rComplete = FALSE;
+		gComplete = FALSE;
+		bComplete = FALSE;
 	}
 	
 	glClearColor(r, g, b, 1.0f);
@@ -110,10 +188,15 @@ void keyOperations()
 
 void init()
 {
+	srand(time(NULL));
 	r = 1.0f;
 	g = 0.0f;
 	b = 0.0f;
 	colorCase = 0;
+	rComplete = FALSE;
+	gComplete = FALSE;
+	bComplete = FALSE;
+	generateV();
 }
 
 int main(int argc, char** argv)
@@ -124,7 +207,7 @@ int main(int argc, char** argv)
 	glutInitWindowSize(640, 480);
 	glutInitWindowPosition(50,50);
 	glutCreateWindow("OpenGL");
-//	glutFullScreen();
+	glutFullScreen();
 	glutDisplayFunc(draw);
 	glutKeyboardFunc(keyPressed);
 	glutKeyboardUpFunc(keyUp);
