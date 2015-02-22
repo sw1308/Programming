a832f87eb13027798ac5c8f63551111459ff5d0c
sw1308 <sw1308@york.ac.uk>
2015-02-22 01:03:44 +0000

    gengine changes

--- C/src/gengine.c
+++ C/src/gengine.c
@@ -13,12 +13,20 @@
 #define TRUE 1
 #define FALSE 0
 
-GLuint VBO; //Vertex Buffer Object
-GLuint VCO; //Vertex Colour Object
+GLfloat Positions[] = 
+	{0.0f, 0.5f,
+	-0.5f, -0.5f,
+	0.5f, -0.5f}; //Array of vertices
+GLfloat Colours[] = 
+	{1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 1.0f}; //Array of colours
+/*GLuint VBO; //Vertex Buffer Object*/
+/*GLuint VCO; //Vertex Colour Object*/
 int keyStates[256]; //Key buffer
 
 //2D rotation
-GLfloat * 2Drotatecw(GLfloat *position, GLfloat rad)
+void rotateCW2D(GLfloat *position, GLfloat rad)
 {
 	int i;
 	
@@ -27,11 +35,9 @@ GLfloat * 2Drotatecw(GLfloat *position, GLfloat rad)
 		position[i] = (position[i] * cos(rad)) + (position[i+1] * sin(rad));
 		position[i+1] = (position[i+1] * cos(rad)) - (position[i] * sin(rad));
 	}
-	
-	return position;
 }
 
-GLfloat * 2Drotateccw(GLfloat *position, GLfloat rad)
+void rotateCCW2D(GLfloat *position, GLfloat rad)
 {
 	int i;
 	
@@ -40,26 +46,24 @@ GLfloat * 2Drotateccw(GLfloat *position, GLfloat rad)
 		position[i] = (position[i] * cos(rad)) - (position[i+1] * sin(rad));
 		position[i+1] = (position[i+1] * cos(rad)) + (position[i] * sin(rad));
 	}
-	
-	return position;
 }
 
 //2D translation
-2Dtranslate(GLfloat *position, GLfloat x, GLfloat y)
+translate2D(GLfloat *position, GLfloat x, GLfloat y)
 {
-	
+	;
 }
 
 //2D scale
-2Dscale(GLfloat *position, GLfloat scale)
+scale2D(GLfloat *position, GLfloat scale)
 {
-	
+	;
 }
 
 //2D shear
-shear(GLfloat *position, GLfloat scale)
+shear2D(GLfloat *position, GLfloat scale)
 {
-	
+	;
 }
 
 //Initial draw function
@@ -78,9 +82,22 @@ void draw()
 }
 
 void redraw() {
-	glutTimerFunc(16, redraw, 0); //Set up next call to redraw
+//	glutTimerFunc(100, redraw, 0); //Set up next call to redraw
 	glClear(GL_COLOR_BUFFER_BIT); //Clear the screen
-		
+	
+/*	glBindBuffer(GL_ARRAY_BUFFER, VBO);*/
+/*	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Positions, GL_DYNAMIC_DRAW);*/
+/*	glFinish();*/
+/*	glVertexPointer(2, GL_FLOAT, 0, (char *) NULL);*/
+/*	*/
+/*	glGenBuffers(1, &VCO);*/
+/*	glBindBuffer(GL_ARRAY_BUFFER, VCO);*/
+/*	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Colours, GL_DYNAMIC_DRAW);*/
+/*	glFinish();*/
+/*	glColorPointer(3, GL_FLOAT, 0, (char *) NULL);*/
+	
+	glutSwapBuffers();
+	glutPostRedisplay();
 }
 
 void keyPressed(unsigned char key, int x, int y)
@@ -101,6 +118,11 @@ void keyOperations()
 	{
 		exit(0);
 	}
+	if (keyStates['a'] == TRUE)
+	{
+		glutTimerFunc(100, redraw, 0); //Set up next call to redraw
+		keyStates['a'] = FALSE;
+	}
 }
 
 //Initialise callback functions
@@ -109,34 +131,25 @@ static void InitGlutCallbacks()
 	glutDisplayFunc(draw);
 	glutKeyboardFunc(keyPressed);
 	glutKeyboardUpFunc(keyUp);
-	glutTimerFunc(16, redraw, 0);
+	glutTimerFunc(100, redraw, 0);
 	glutTimerFunc(10, keyOperations, 0);
 }
 
-//Create objects
-static void CreateVertexBuffer()
-{
-	const GLfloat Positions[] = 
-	{0.0f, 0.5f,
-	-0.5f, -0.5f,
-	0.5f, -0.5f};
-	const GLfloat Colours[] = 
-	{1.0f, 0.0f, 0.0f,
-	0.0f, 1.0f, 0.0f,
-	0.0f, 0.0f, 1.0f};
-
- 	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Positions, GL_STATIC_DRAW);
-	glFinish();
-	glVertexPointer(2, GL_FLOAT, 0, (char *) NULL);
-	
-	glGenBuffers(1, &VCO);
-	glBindBuffer(GL_ARRAY_BUFFER, VCO);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Colours, GL_STATIC_DRAW);
-	glFinish();
-	glColorPointer(3, GL_FLOAT, 0, (char *) NULL);
-}
+/*//Create objects*/
+/*static void CreateVertexBuffer()*/
+/*{*/
+/* 	glGenBuffers(1, &VBO);*/
+/*	glBindBuffer(GL_ARRAY_BUFFER, VBO);*/
+/*	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Positions, GL_DYNAMIC_DRAW);*/
+/*	glFinish();*/
+/*	glVertexPointer(2, GL_FLOAT, 0, (char *) NULL);*/
+/*	*/
+/*	glGenBuffers(1, &VCO);*/
+/*	glBindBuffer(GL_ARRAY_BUFFER, VCO);*/
+/*	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), Colours, GL_DYNAMIC_DRAW);*/
+/*	glFinish();*/
+/*	glColorPointer(3, GL_FLOAT, 0, (char *) NULL);*/
+/*}*/
 
 //Main function
 int main(int argc, char** argv)
@@ -146,7 +159,7 @@ int main(int argc, char** argv)
 	glutInitWindowPosition(50, 50);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutCreateWindow("OpenGL Tutorial");
-	glutFullScreen();
+	//glutFullScreen();
 	
 	InitGlutCallbacks();
 	
@@ -157,7 +170,6 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	
-	CreateVertexBuffer();
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glutMainLoop();
 }
