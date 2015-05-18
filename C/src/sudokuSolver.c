2aca2fe537f611d92e3124ee6cff97a298b7b985
sw1308 <sw1308@york.ac.uk>
2015-05-18 20:37:18 +0100

    Need some planning for sudokuSolver

--- C/src/sudokuSolver.c
+++ C/src/sudokuSolver.c
@@ -1,11 +1,11 @@
 //while (not solved):
 //	while (change happens):
 //		fill in determined possibilities
-//	
+//
 //	add branch (current table)
 //	current table = new branch
 //	fill in (possibility by heuristic)
-	
+
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -33,50 +33,49 @@ char turnOff(int cell, int x)
 void setCell(TABLE* t, char coords, char val)
 {
 	int i;
-	
+
 	for(i=0;i<16;i++)
 	{
 		t[coords>>4][count&16] = turnOff(t[coords>>4][count&16],i);
 	}
-	
+
 	for(i=0;i<16;i++)
 	{
 		if(!(i==(coords>>4)))
 		{
 			t[i][coords&16] = turnOff(t[i][coords&16], val);
 		}
-		
+
 		if(!(i==(coords&16)))
 		{
 			t[coords>>4][i] = turnOff(t[coords>>4][i], val);
 		}
 	}
-	
+
 	//TODO account for supercell
+}
+
+char check4SingleRow(char i, char j)
+{
+	;
 }
 
 int main()
 {
 	init();
-	
+
 	char i;
 	char j;
-	
+
 	while(complete < 1)
 	{
-		for(i=1;i<17;i++)
+		while(changeHappens > 1)
 		{
-			for(j=0;j<16;j++)
-			{
-				char check = check4SingleRow(i,j);
-				
-				if(check>0)
-				{
-					setCell(currentTable, (check << 4)+j, i);
-				}
-			}
+			applyRules(table);
 		}
+		forkTree();
+		fillRandon();
 	}
-	
+
 	printf("Puzzle solved");
 }
