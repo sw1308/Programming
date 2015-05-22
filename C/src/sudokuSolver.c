a0cfb19fd344b3013df6bfb8bdb9a3403f66e00a
sw1308 <sw1308@york.ac.uk>
2015-05-22 03:46:35 +0100

    Added some comments to make it easier to pick up where I left off

--- C/src/sudokuSolver.c
+++ C/src/sudokuSolver.c
@@ -1,16 +1,26 @@
-//while (not solved):
-//	while (change happens):
-//		fill in determined possibilities
-//
-//	add branch (current table)
-//	current table = new branch
-//	fill in (possibility by heuristic)
+/*			SUDOKU SOLVER
 
+This program aims to solve a 16x16 sudoku puzzle quickly and efficiently.
+
+Currently the program can be summarized by taking the following steps:
+
+while (not solved):
+	while (change happens):
+		fill in possibilities determined by rules of sudoku
+
+	if (failure occurred):
+		revert to parent table
+
+	create new branch in problem tree
+	switch to new branch
+	fill in random cell with one of it's possible values
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
 
 char complete = 0;
+char changed = 1;
 
 typedef struct TABLE
 {
@@ -24,41 +34,64 @@ void init(int table[16][16]) {
 	currentTable = table;
 }
 
-char turnOff(int cell, int x)
+void turnOff(int cell, int x)
 {
-	count[x]--;
+	currentTable.count[x]--; //Decrement count of value in currentTable
+	changed++; //Indicate that a change to the table has happened
 	return cell & !(1 << x-1);
 }
 
-void setCell(TABLE* t, char coords, char val)
+//Set cell at coords to val
+void setCell(char coords, char val)
+{
+	char i;
+
+	for(i=0;i<16;i++)
+	{
+		switch val
+		{
+			case i:
+				break;
+			default:
+				turnOff(cell, i);
+		}
+	}
+}
+
+//Code kept for applying ruleset, not currently used
+/*void setCell(char coords, char val)
 {
 	int i;
 
+	//Set only possibility for cell to val
 	for(i=0;i<16;i++)
 	{
-		t[coords>>4][count&16] = turnOff(t[coords>>4][count&16],i);
+		switch val
+		{
+			case i:
+				break;
+			default:
+				currentTable.cells[coords>>4][coords&16] = turnOff(currentTable.cells[coords>>4][coords&16],i);
 	}
 
+	//Remove all val possibilities in row and column of cell
 	for(i=0;i<16;i++)
 	{
-		if(!(i==(coords>>4)))
+		switch i
 		{
-			t[i][coords&16] = turnOff(t[i][coords&16], val);
-		}
-
-		if(!(i==(coords&16)))
-		{
-			t[coords>>4][i] = turnOff(t[coords>>4][i], val);
+			case (coords>>4):
+				break;
+			case (coords&16):
+				break;
+			default:
+				currentTable.cells[i][coords&16] = turnOff(currentTable.cells[i][coords&16],val);
+				currentTable.cells[coords>>4][i] = turnOff(currentTable.cells[coords>>4][i],val);
 		}
 	}
 
-	//TODO account for supercell
-}
-
-char check4SingleRow(char i, char j)
-{
-	;
-}
+	//Remove all val possibilities in supercell of cell
+	//TODO
+}*/
 
 int main()
 {
