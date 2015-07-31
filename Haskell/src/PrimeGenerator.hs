e7aab587119d1d152eaba8b406c9532b7ea914da
sw1308 <sw1308@york.ac.uk>
2015-07-31 20:17:25 +0100

    Did some work on PrimeGenerator.hs

--- Haskell/src/PrimeGenerator.hs
+++ Haskell/src/PrimeGenerator.hs
@@ -1,18 +1,25 @@
 primeList :: [Integer]
-primeList = [2]
+primeList = [2,2]
 
 test :: Integer -> [Integer] -> Bool
 test num [] = True
-test num [x:xs]
-	| x > sqrt num = True
+test num (x:xs)
+	| (fromInteger x) > sqrt (fromInteger num) = True
 	| mod num x == 0 = False
 	| otherwise = test num xs
 
-nextPrime :: [Integer] -> Integer
-nextPrime 1 [] = 2
+nextPrime :: Integer -> [Integer] -> [Integer]
+nextPrime 1 [] = [2]
 nextPrime count list
-	| test count list == True = count
-	| test count list == False = nextPrime count + 1 list
+	| test count list == True = [count] ++ list ++ [count]
+	| test count list == False = nextPrime (count + 1) list
 
-main :: [Integer] -> [Integer]
-main = primeList ++ [nextPrime last primeList + 1 primeList]
+getPrime :: Integer -> [Integer] -> Integer
+getPrime 1 list = last list
+getPrime x (current:list) = getPrime (x-1) (nextPrime (current+1) list)
+
+main = do
+			putStrLn "Which prime would you like? "
+			n <- getLine
+			let prime = getPrime (read n::Integer)
+			print prime
